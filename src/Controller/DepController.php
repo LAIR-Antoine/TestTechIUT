c26a761b3072c497834a13c57615af5bd56e516f
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2022-06-07 11:37:31 +0200

    Foreach Twig Departements

--- src/Controller/DepController.php
+++ src/Controller/DepController.php
@@ -10,9 +10,31 @@ class DepController extends AbstractController
     #[Route("/departements", name:"app_dep")]
     public function dep()
     {
+        $departements =  [
+            [
+                'nom' => 'Métiers du Multimédia et de l\'internet',
+                'sigle' => 'MMI',
+                'logo' => 'https://www.iut-troyes.univ-reims.fr/assets/img/logo_191.jpg',
+                'presentation' => 'Le département Métiers de l’Internet et du Multimédia (anciennement Services et Réseaux de Communication) forme les acteurs de l’Internet, des médias numériques, de la communication plurimédia, de la création graphique.',
+                'responsable' => 'Florent Libbrecht',
+                'mail_responsable' => 'florent.libbrecht@univ-reims.fr'
+            ],
+            [
+                'nom' => 'Carrière Juridique',
+                'sigle' => 'CJ',
+                'logo' => 'https://www.iut-troyes.univ-reims.fr/assets/img/logo_196.jpg',
+                'presentation' => 'Le département Carrières Juridiques prépare les étudiants à devenir des techniciens du droit dans des domaines variés : études de notaires, d’huissiers mais aussi des cabinets d’expert-comptable ou des services de ressources humaines.',
+                'responsable' => 'Gilles Darmon',
+                'mail_responsable' => 'gilles.darmon@univ-reims.fr'
+            ]
+        ];
+        
 
         return $this->render(
-            'dep.html.twig'
+            'dep.html.twig',
+            [
+                'departements' => $departements,
+            ]
         );
         
     }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,44 @@ class HomeController extends AbstractController
     public function home()
     {
 
+        $actualites = [
+            [
+                'titre' => 'Concours plaidoiries 2022',
+                'date' => '25/05/2022',
+                'image' => 'image1.png' //image à remplacer
+            ],
+            [
+                'titre' => 'Rénovation énergétique de l\'IUT',
+                'date' => '20/05/2022',
+                'image' => 'image2.png' //image à remplacer
+            ],
+            [
+                'titre' => 'Afterwork GMP & futur Job Dating',
+                'date' => '18/05/2022',
+                'image' => 'image3.png' //image à remplacer
+            ],
+            [
+                'titre' => 'L4M : Recrutements Experts',
+                'date' => '16/05/2022',
+                'image' => 'image4.png' //image à remplacer
+            ],
+            [
+                'titre' => 'CROUS : ateliers d\'éloquence',
+                'date' => '27/04/2022',
+                'image' => 'image2.png' //image à remplacer
+            ],
+            [
+                'titre' => 'Vacances de Printemps : fermeture de l\'IUT',
+                'date' => '08/04/2022',
+                'image' => 'image3.png' //image à remplacer
+            ],
+        ];
+
         return $this->render(
-            'home.html.twig'
+            'home.html.twig',
+            [
+                'actus' => $actualites,
+            ]
         );
         
     }
