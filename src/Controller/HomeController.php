eecd23e837c16a30f6e1b6ffab93ac00962391f6
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2022-06-07 20:30:50 +0200

    Add events and actus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,32 +15,50 @@ class HomeController extends AbstractController
             [
                 'titre' => 'Concours plaidoiries 2022',
                 'date' => '25/05/2022',
-                'image' => 'image1.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-plaidoiries2022.jpg'
             ],
             [
                 'titre' => 'Rénovation énergétique de l\'IUT',
                 'date' => '20/05/2022',
-                'image' => 'image2.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-Actu-renovationenergetique2022.jpg'
             ],
             [
                 'titre' => 'Afterwork GMP & futur Job Dating',
                 'date' => '18/05/2022',
-                'image' => 'image3.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-afterworkGMP-mai2022.jpg'
             ],
             [
                 'titre' => 'L4M : Recrutements Experts',
                 'date' => '16/05/2022',
-                'image' => 'image4.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-salonL4M-juin2022.jpg'
             ],
             [
                 'titre' => 'CROUS : ateliers d\'éloquence',
                 'date' => '27/04/2022',
-                'image' => 'image2.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-Atelierseloquences-CROUS2022.jpg'
             ],
             [
                 'titre' => 'Vacances de Printemps : fermeture de l\'IUT',
                 'date' => '08/04/2022',
-                'image' => 'image3.png' //image à remplacer
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-vacancesprintemps.jpg'
+            ],
+        ];
+
+        $evenements = [
+            [
+                'titre' => 'Concours plaidoiries 2022',
+                'date' => '25/05/2022',
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-plaidoiries2022.jpg'
+            ],
+            [
+                'titre' => 'Rénovation énergétique de l\'IUT',
+                'date' => '20/05/2022',
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-Actu-renovationenergetique2022.jpg'
+            ],
+            [
+                'titre' => 'Afterwork GMP & futur Job Dating',
+                'date' => '18/05/2022',
+                'image' => 'http://www.iut-troyes.univ-reims.fr/media/images/2022/Visuel-afterworkGMP-mai2022.jpg'
             ],
         ];
 
@@ -48,6 +66,7 @@ class HomeController extends AbstractController
             'home.html.twig',
             [
                 'actus' => $actualites,
+                'events' => $evenements,
             ]
         );
         
